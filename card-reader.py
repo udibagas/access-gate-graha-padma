ce5a919784ea0060db3defb86fbc394c3caddec9
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-02-08 12:00:05 +0700

    update

---
+++ card-reader.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from rfidhid.core import RfidHid
+
+try:
+    # Try to open RFID device using default vid:pid (ffff:0035)
+    rfid = RfidHid()
+except Exception as e:
+    print(e)
+    exit()
+
+payload_response = rfid.read_tag()
+uid = payload_response.get_tag_uid()
+
+rfid.beep()
+print(uid)
