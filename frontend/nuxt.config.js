e92b091f261d5f918f0f937f9c4dcb70057cb1bf
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-12-09 14:59:13 +0700

    add baseurl

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -48,7 +48,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    // baseURL: process.env.API_URL || 'http://localhost:8000',
+    baseURL: process.env.API_URL || 'http://localhost:8000',
     credentials: true
   },
 
