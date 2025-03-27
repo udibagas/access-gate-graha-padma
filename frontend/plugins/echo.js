88008e3cab59107f0b12f812e3aa77c1c1200230
Bagas Udi Sahsangka <udibagas@gmail.com>
2025-03-27 17:46:22 +0700

    fix: ambil host dari location

--- frontend/plugins/echo.js
+++ frontend/plugins/echo.js
@@ -5,7 +5,7 @@ window.Pusher = require('pusher-js')
 window.echo = new Echo({
   broadcaster: 'reverb',
   key: 'g2iix2c1zwlt3dgvo35h',
-  wsHost: '127.0.0.1', // sesuaikan ini sesuai ip server-nya
+  wsHost: window.location.host, // sesuaikan ini sesuai ip server-nya
   wsPort: 8080,
   forceTLS: false,
   disableStats: true,
